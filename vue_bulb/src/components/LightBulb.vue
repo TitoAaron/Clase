fff280f912b951a4dd38f70ae0fa63f1ad1c109e
TitoAaron <titoaaron13@gmail.com>
2022-04-12 11:17:43 +0200

    bulblight

---
+++ vue_bulb/src/components/LightBulb.vue
@@ -0,0 +1,43 @@
+
+<script>
+export default {
+    props:["contador"],
+    data(){
+        return{
+            cargas : this.contador,
+            isOn: false
+        }
+    },methods: {
+        aumentarContador(){
+            if(this.cargas != 0){
+               this.cargas--;
+            }
+            else 
+                this.isOn = true;
+        }
+    }
+}
+</script>
+
+<template>
+    <div @click="aumentarContador" class="bulb" :class="[isOn ? 'bulb-on' : 'bulb-off']">
+       {{cargas}}
+    </div>
+</template>
+
+<style>
+.bulb-off {
+    width: 265px;
+    height: 400px;
+    background: url('lightoff.png') no-repeat;
+    margin: auto;
+}
+
+.bulb-on {
+    width: 265px;
+    height: 400px;
+    background: url('lighton.png') no-repeat;
+    ;
+    margin: auto;
+}
+</style>
