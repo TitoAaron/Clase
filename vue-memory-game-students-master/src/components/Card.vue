215673f857dead882f2eb989029c12951f9fd0d7
TitoAaron <titoaaron13@gmail.com>
2022-04-13 09:33:42 +0200

    pokemons

---
+++ vue-memory-game-students-master/src/components/Card.vue
@@ -0,0 +1,24 @@
+<!-- Card Component -->
+<script setup>
+
+
+const props = defineProps({
+    back: String,
+    front: String,
+    reveal: Boolean,
+})
+
+
+function cambiar(){
+    reveal = !reveal;
+}
+
+</script>
+
+
+
+<template>
+    <img  v-if="!reveal" :src="front" alt="">  
+
+    <img  v-else :src="back" alt="">
+</template>
